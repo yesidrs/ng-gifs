1141ede647c9636e01629f2efe189d7a4ec65464
yesiddev <yesiddeveloper@gmail.com>
2023-05-23 16:10:33 -0500

    add colors based in catpuccin theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      'dark': '#11111b',
+      'white': '#ffffff',
+      'light': '#d7dae0',
+      'background': '#1e1e2e',
+      'purple': '#cba6f7',
+      'green': '#a6e3a1',
+      'orange': '#fab387',
+      'blue': '#89dceb',
+      'red': '#f38ba8',
+    }
   },
   plugins: [],
 };
+
+// por si no me convence el otro red ##e38c8f
