1d7d5a31494ca75c70efb73012d10d4479506126
yesiddev <yesiddeveloper@gmail.com>
2023-05-24 19:02:00 -0500

    add ngFor

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
+  constructor(private gifsService: GifsService) {}
 
+  get tagsHistory() {
+    return this.gifsService.tagsHistory;
+  }
 }
