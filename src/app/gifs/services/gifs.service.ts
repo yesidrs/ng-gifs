981a32d60b671ed7c6641332b17f6d8667d927d3
yesiddev <yesiddeveloper@gmail.com>
2023-05-25 13:42:21 -0500

    add http get method

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,21 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+const API_KEY: string = 'VEvtJogPIK5tkjGaANU8E755Rdf8cxXQ';
+const URL: string = 'https://api.giphy.com/v1/gifs';
+const LIMIT: number = 15;
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
   private _tagsHistory: string[] = [];
+  private _gifs: Gif[] = [];
+
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string): void {
+  get gifs() {
+    return [...this._gifs];
+  }
+
+  searchGifs(tag: string): void {
     this.duplicatedTags(tag);
+    this.limitTagsHistory();
 
     this._tagsHistory.unshift(tag);
 
-    this.limitTagsHistory();
+    const params = new HttpParams()
+      .set('api_key', API_KEY)
+      .set('q', tag)
+      .set('limit', LIMIT.toString());
+
+     this.http
+      .get<SearchResponse>(`${URL}/search`, { params })
+      .subscribe((response) => {
+        this._gifs = response.data;
+      });
   }
 
   private duplicatedTags(tag: string): void {
