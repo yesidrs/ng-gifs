7403507e29f592baaee18bb089206e0837ea4253
yesiddev <yesiddeveloper@gmail.com>
2023-05-26 15:19:34 -0500

    appy DRY principle

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardComponent } from './components/card/card.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent, CardComponent],
+  declarations: [HomePageComponent, SearchBoxComponent, CardComponent, CardListComponent],
   exports: [HomePageComponent],
   imports: [CommonModule],
 })
